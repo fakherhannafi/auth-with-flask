f65106d0ea5e28f1cb280f64ed09ab6f452f2bc8
fakhrovski <Fakhrovski@DESKTOP-LDMK5KF>
2019-02-09 23:58:44 +0100

    add getSales API

--- app.py
+++ app.py
@@ -1,25 +1,45 @@
 from flask import Flask
 from flask import Flask, flash, redirect, url_for, render_template, request, session, abort
-import os
+import os,json
 from passlib.apps import custom_app_context as pwd_context
 from sqlalchemy.orm import sessionmaker
 from models import *
+import sqlalchemy as db 
 
-engine = create_engine(
+""" engine = create_engine(
     'mysql+pymysql://root@localhost/mydatabase?charset=utf8',
     connect_args = {
         'port': 3306
     },
     echo='debug',
     echo_pool=True
-)
- 
+) """
+
+engine = db.create_engine('mysql+pymysql://root@localhost/mydatabase?charset=utf8')
+connection = engine.connect()
+metadata = db.MetaData()
+
 app = Flask(__name__)
  
+
+@app.route('/getSales')
+def getsales():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        sales = db.Table('sales', metadata, autoload=True, autoload_with=engine)
+        query = db.select([sales])
+        keys=sales.columns.keys()
+        resultset = connection.execute(query).fetchall()
+        items=[]
+        for row in resultset:
+                items.append({keys[0]: row[0],keys[1]: row[1],keys[2]: row[2],keys[3]: row[3], })
+    
+        connection.close()
+        return json.dumps({'items':items})
 @app.route('/')
 def home():
     if not session.get('logged_in'):
-        print ('login failed')
         return render_template('login.html')
     else:
         return render_template('welcome.html')
--- models.py
+++ models.py
@@ -8,9 +8,7 @@ engine = create_engine(
     'mysql+pymysql://root@localhost/mydatabase?charset=utf8',
     connect_args = {
         'port': 3306
-    },
-    echo='debug',
-    echo_pool=True
+    }
 )
 Base = declarative_base()
  
